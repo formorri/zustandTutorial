7e9dc1259501b59f8fb89ccf325ec96bfe726ca3
zixin.lam <zixin.lam@up-devlabs.io>
2024-02-20 16:21:48 +0800

    first commit

---
+++ src/components/Column.jsx
@@ -0,0 +1,71 @@
+import "./Column.css"
+import Task from "./Task"
+import { useStore } from "../store"
+import { useState } from "react"
+import classNames from 'classnames';
+
+const Column = ({ state }) => {
+  const [text, setText] = useState("")
+  const [open, setOpen] = useState(false)
+  const [drop, setDrop] = useState(false)
+  const tasks = useStore((store) => store.tasks.filter((task) => task.state == state))
+
+  const addTask = useStore((store) => store.addTask)
+
+  const setDraggedTask = useStore((store) => store.setDraggedTask)
+  const draggedTask = useStore((store) => store.draggedTask)
+  const moveTask = useStore((store) => store.moveTask)
+  return (
+    <div
+       className={classNames('column', { drop: drop })}
+      onDragOver={(e) => {
+        e.preventDefault()
+        setDrop(true)
+      }}
+      
+      onDragLeave={(e) => {
+        e.preventDefault()
+        setDrop(false)
+      }}
+      
+      onDrop={(e) => {
+        moveTask(draggedTask, state)
+        setDraggedTask(null)
+        setDrop(false)
+        // console.log(draggedTask)
+      }}
+    >
+      <div className="titleWrapper">
+        <p>{state}</p>
+        <button
+          onClick={() => {
+            setOpen(true)
+          }}
+        >
+          Add
+        </button>
+      </div>
+      {tasks.map((task) => (
+        <Task key={task.title} title={task.title}></Task>
+      ))}
+      {open && (
+        <div className="Modal">
+          <div className="modalContent">
+            <input onChange={(e) => setText(e.target.value)} value={text} />
+            <button
+              onClick={() => {
+                addTask(text, state)
+                setText("")
+                setOpen(false)
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Column
---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { create } from "zustand"
+const store = (set) => ({
+  tasks: [{ title: "testTask", state: "ONGOING" }],
+
+  addTask: (title, state) => set((store) => ({ tasks: [...store.tasks, { title, state }] })),
+  deleteTask: (title) => set((store) => ({ tasks: store.tasks.filter((task) => task.title !== title) })),
+  draggedTask: null,
+  setDraggedTask: (title) => set({ draggedTask: title }),
+  moveTask: (title, state) => 
+    set((store) => ({ 
+      tasks: store.tasks.map((task) => 
+      (task.title === title ? {title, state} : task)) 
+    })),
+})
+
+export const useStore = create(store)
